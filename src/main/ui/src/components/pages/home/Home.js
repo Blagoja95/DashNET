51562e6331a6bee81cd3e665c4ca5c325e5b5771
Ognjen Vujasinovic <ognjen.vujasinovic@develabs.net>
2023-12-07 15:44:51 +0100

    29: Card Component
    Created 2 cards on home page with data about active project and stats.

--- src/main/ui/src/components/pages/home/Home.js
+++ src/main/ui/src/components/pages/home/Home.js
@@ -2,35 +2,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { resetInitialised, uiActions } from '../../../store/slices/uiSlice';
 import Header from '../../layout/header/Header';
+import { Box } from '@mui/material';
+import TeamCard from './TeamCard';
+import InfoCard from './InfoCard';
 
-const Home = () =>
-{
+const Home = () => {
 	const dispatch = useDispatch();
 
 	const isInitialised = useSelector(state => state.ui.initialised);
 
-	useEffect(() =>
-	{
+	useEffect(() => {
 		dispatch(uiActions.setInitialised(true));
 		dispatch(resetInitialised());
-	}, [ dispatch ]);
+	}, [dispatch]);
 
-	useEffect(() =>
-	{
+	useEffect(() => {
 		console.log(isInitialised);
-	}, [ isInitialised ]);
+	}, [isInitialised]);
 
 	return (
 		<>
 			<header>
 				<Header
-					subtitleTxt={ 'Track your projects, tasks & team activity here' }
-					titleTxt={ 'Manage your projects' }
+					subtitleTxt={'Track your projects, tasks & team activity here'}
+					titleTxt={'Manage your projects'}
 				/>
 			</header>
 
 			<main>
-
+				<Box sx={{ display: 'flex', gap: 5 }}>
+					<TeamCard />
+					<InfoCard />
+				</Box>
 			</main>
 		</>
 	)
---
+++ src/main/ui/src/components/pages/home/InfoCard.js
@@ -0,0 +1,33 @@
+import { Box, Card, CardActions, CardContent, Typography } from "@mui/material"
+
+const InfoCard = () => {
+	return (
+		<Card sx={{ minWidth: 275, bgcolor: 'gray.dark', color: 'white.main', flex: 1, borderRadius: 5, p: 2, display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+			<CardContent>
+				<Typography variant='h3' sx={{ color: 'white.light' }}>Overall information</Typography>
+				<Box sx={{ display: 'flex', alignItems: 'flex-end', gap: 1, my: 5 }}>
+					<Typography variant='h3' sx={{ color: 'white.light' }}>57 / 125</Typography>
+					<Typography variant='body2'>tasks done</Typography>
+				</Box>
+			</CardContent>
+			<Box sx={{ display: 'flex', gap: 5 }}>
+				<Box
+					sx={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', bgcolor: 'error.main', color: 'white.light', borderRadius: 3, py: 2 }}>
+					<Typography variant='h4' sx={{ py: 5 }}>30</Typography>
+					<Typography variant='body1'>Not started</Typography>
+				</Box>
+				<Box
+					sx={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', bgcolor: 'info.main', color: 'white.light', borderRadius: 3, py: 2 }}>
+					<Typography variant='h4' sx={{ py: 5 }}>36</Typography>
+					<Typography variant='body1'>In Progress</Typography>
+				</Box>
+				<CardActions
+					sx={{ flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', bgcolor: 'success.light', color: 'white.light', borderRadius: 3, py: 2 }}>
+					<Typography variant='h4' sx={{ py: 5 }}>57</Typography>
+					<Typography variant='body1'>Done</Typography>
+				</CardActions>
+			</Box>
+		</Card>
+	)
+}
+export default InfoCard
