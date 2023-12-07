7dde9f068eae3ddc84b8accc022667fdd914da78
Ognjen Vujasinovic <38105408+ogi1998@users.noreply.github.com>
2023-12-07 09:56:58 +0100

    19: Navigation Menu (#26)
    * 19: Navigation Menu
    
    Created `Navigation` component and adjusted it to the layout

--- src/main/ui/src/components/layout/navigation/Navigation.js
+++ src/main/ui/src/components/layout/navigation/Navigation.js
@@ -1,6 +1,86 @@
+import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import { Home, AssignmentTurnedIn, Dashboard, BarChart, Group, Settings, ChevronLeft, ChevronRight } from '@mui/icons-material';
+import { NavLink, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useTheme } from "@emotion/react";
+
 const Navigation = () => {
+	const { pathname } = useLocation();
+	const theme = useTheme();
+
+	const [isFull, setIsFull] = useState(true);
+
+	const navItems = [
+		{ text: 'Home', icon: Home, to: '/' },
+		{ text: 'My Tasks', icon: AssignmentTurnedIn, to: '/tasks' },
+		{ text: 'My Projects', icon: Dashboard, to: '/projects' },
+		{ text: 'Statistics', icon: BarChart, to: '/statistics' },
+		{ text: 'Users', icon: Group, to: '/users' },
+		{ text: 'Settings', icon: Settings, to: '/settings' }
+	];
+
 	return (
-		<div>Navigation</div>
+		<Drawer
+			variant='permanent'
+			sx={{
+				'& .MuiPaper-root':
+				{
+					bgcolor: 'gray.dark',
+					position: 'relative',
+					overflowX: 'hidden',
+					height: '100vh',
+					'&::-webkit-scrollbar': {
+						display: 'none'
+					},
+					transition: theme.transitions.create('width', {
+						easing: theme.transitions.easing.sharp,
+						duration: isFull ? theme.transitions.duration.leavingScreen : theme.transitions.duration.enteringScreen
+					}),
+					width: !isFull ? `calc(${theme.spacing(12)} + 1px)` : '25vw'
+				}
+			}}>
+			<List sx={{
+				pt: '2rem',
+				px: '1rem'
+			}}>
+				<Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mb: '5rem' }}>
+					<Typography variant='h5' sx={{ textAlign: 'center', px: '5rem', color: 'white.main', display: isFull ? 'block' : 'none', flex: 1 }}>
+						Dash
+						<Box component='span' sx={{ fontWeight: 'bold', color: 'primary.main' }}>NET</Box>
+					</Typography>
+					<Box
+						onClick={() => setIsFull(el => !el)}
+						component='span'
+						sx={{
+							borderRadius: '5px',
+							cursor: 'pointer',
+							p: '0.5rem',
+							'&:hover': { bgcolor: 'gray.main' }
+						}}
+					>
+						{isFull ? <ChevronLeft sx={{ fontSize: '3rem', color: 'white.main' }} /> : <ChevronRight sx={{ fontSize: '3rem', color: 'white.main' }} />}
+					</Box>
+				</Box>
+				{
+					navItems.map((el, idx) =>
+						<ListItemButton LinkComponent={NavLink} to={el.to} key={idx}
+							sx={{
+								my: '2rem',
+								py: '1rem',
+								borderRadius: '5px',
+								gap: '25%',
+								bgcolor: el.to === pathname && 'gray.main',
+								'&:hover': { bgcolor: 'gray.main' },
+								'&:hover *': { color: 'primary.main' }
+							}}>
+							<ListItemIcon>
+								<el.icon sx={{ fontSize: '2rem', color: el.to === pathname ? 'primary.main' : 'white.main' }} />
+							</ListItemIcon>
+							{isFull && <ListItemText primary={el.text} primaryTypographyProps={{ color: el.to === pathname ? 'primary.main' : 'white.main', fontSize: '1.5rem' }} />}
+						</ListItemButton>
+					)}
+			</List>
+		</Drawer>
 	)
 }
 export default Navigation
