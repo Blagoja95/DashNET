aba71411846ce7f9269368d96a39e553974486f1
Boris Blagojevic <boris.blagojevic@develabs.net>
2023-12-08 13:04:42 +0100

    #27 - Grid component
    Introducing Grid component for Tasks
    Introducing UserAvatar component

--- src/main/ui/src/components/layout/header/Header.js
+++ src/main/ui/src/components/layout/header/Header.js
@@ -1,11 +1,11 @@
-import {useState, useRef, useEffect} from 'react';
+import { useState, useRef, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import {Box, Avatar, Typography, InputAdornment, TextField} from '@mui/material';
+import { Box, Avatar, Typography, InputAdornment, TextField } from '@mui/material';
+import UserAvatar from '../../ui/avatar/UserAvatar';
 
-const Header = ({titleTxt, subtitleTxt, hideSearchBar = false}) =>
+const Header = ({ titleTxt, subtitleTxt, hideSearchBar = false }) =>
 {
-	const [isFocused, setFocused] = useState(false);
+	const [ isFocused, setFocused ] = useState(false);
 	const inputRef = useRef(null);
 
 	const inputWidth = isFocused ? 300 : 30;
@@ -21,74 +21,83 @@ const Header = ({titleTxt, subtitleTxt, hideSearchBar = false}) =>
 			inputRef.current.blur();
 		}
 
-	}, [isFocused]);
+	}, [ isFocused ]);
 
 	return <Box
 		component={ 'header' }
-		sx={{
+		sx={ {
 			p: '1rem',
 			display: 'flex',
 			flexDirection: 'column'
-		}}>
+		} }>
 
 		<Box
-			sx={{
+			sx={ {
 				display: 'flex',
 				justifyContent: 'space-between'
-			}}>
+			} }>
 
 			<Typography variant='h3'
-						sx={{
+						sx={ {
 							fontWeight: 'bold'
-						}}>
+						} }>
 
-				{titleTxt?.length > 0 ? titleTxt : 'Dashboard'}
+				{ titleTxt?.length > 0 ? titleTxt : 'Dashboard' }
 			</Typography>
 
 			<Box
-				sx={{
+				sx={ {
 					display: 'flex',
 					alignItems: 'center',
 					gap: 4
-				}}>
+				} }>
 
 				<TextField
-					inputRef={inputRef}
-					sx={{
+					inputRef={ inputRef }
+					sx={ {
 						width: inputWidth,
 						transition: 'width 0.3s ease-in-out',
-						display: `${hideSearchBar ? 'none' : 'block'}`
-					}}
-					InputProps={{
+						display: `${ hideSearchBar ? 'none' : 'block' }`
+					} }
+					InputProps={ {
 						startAdornment: (
 							<InputAdornment position='start'>
 								<SearchIcon
-									sx={{
+									sx={ {
 										cursor: 'pointer'
-									}}/>
+									} }/>
 							</InputAdornment>
 						),
-					}}
+					} }
 					variant='standard'
-					onClick={() => setFocused(!isFocused)}
-					onBlur={() => setFocused(false)}
+					onClick={ () => setFocused(!isFocused) }
+					onBlur={ () => setFocused(false) }
 				/>
 
-				<Avatar alt='user image' src='' sx={{width: 50, height: 50}}>
-					<AccountCircleIcon sx={{fontSize: 50}}/>
-				</Avatar>
+				<Box
+					sx={ {
+						cursor: 'pointer',
+						border: '2px solid transparent',
+						'&:hover': {
+							border: '2px solid',
+							borderRadius: '999999%'
+						}
+					} }
+				onClick={() => console.log('User menu')}>
+					<UserAvatar data={ { src: 'https://randomuser.me/api/portraits/lego/2.jpg' } }/>
+				</Box>
 			</Box>
 		</Box>
 
 		<Box>
 			<Typography variant='subtitle1'
-						sx={{
-							display: `${(subtitleTxt && subtitleTxt?.length > 0) ? 'block' : 'none'}`,
+						sx={ {
+							display: `${ ( subtitleTxt && subtitleTxt?.length > 0 ) ? 'block' : 'none' }`,
 							color: 'white.main',
 							marginTop: '1rem'
-						}}>
+						} }>
 
-				{subtitleTxt?.length > 0 ? subtitleTxt : 'Subtitle'}
+				{ subtitleTxt?.length > 0 ? subtitleTxt : 'Subtitle' }
 			</Typography>
 		</Box>
 	</Box>
